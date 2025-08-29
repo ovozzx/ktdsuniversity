5beacf2e4a404f4d20e1bd47de5e095883ba2ea1
ovozzx <takiwow19@gmail.com>
2025-08-29 19:31:39 +0900

    250829

---
+++ practice/src/baekjoon/advanced/Problem25083.java
@@ -0,0 +1,17 @@
+package baekjoon.advanced;
+
+public class Problem25083 {
+	
+	public static void main(String[] args) {
+		// " \ => 앞에다가 \ 써주기
+		
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+		
+	}
+
+}
